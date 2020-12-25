b0747ec5fa0bdfad2f81e6861b98731486b13af2
LeonGeldsch <leon.geldschlaeger@gmail.com>
2020-12-25 22:51:14 +0100

    updated slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,9 +1,9 @@
 var slideRightButton = document.querySelector(".btn-right");
 var slideLeftButton = document.querySelector(".btn-left");
 
+var allSlides = document.querySelectorAll(".slideshow-image")
 
-
-
+var tl = gsap.timeline();
 
 /*
 gsap.set(".slideshow-image-1", {
@@ -12,11 +12,73 @@ gsap.set(".slideshow-image-1", {
 */
 
 
+
+
+function startSlideShow() {
+   let index = 0
+    allSlides.forEach(slide => {
+        setTimeout(function () {
+            zeroToOne(slide)
+            oneToTwo(slide, 1)
+            twoToThree(slide, 2)
+            threeToZero(slide, 3)
+            console.log(index);
+        }, index*1000)
+        index++
+    });
+}
+/* zeroToOne(allSlides[0])
+oneToTwo(allSlides[0], 1)
+twoToThree(allSlides[0], 2)
+threeToZero(allSlides[0], 3)
+ */
+
+function zeroToOne (slide, delay = 0) {
+    gsap.to(slide, {
+        x:-percentWidthToPixel(slide.parentNode, 50) + percentWidthToPixel(slide, 50),
+        duration: 1,
+        delay: delay,
+        ease: "power1"
+    })
+}
+
+function oneToTwo (slide, delay = 0) {
+    gsap.set(slide, {
+        zIndex:1,
+        delay: delay
+    })
+    gsap.to(slide, {
+        x:0,
+        width:percentWidthToPixel(slide.parentNode, 50),
+        duration: 1,
+        delay: delay,
+        ease: "power1"
+    })
+}
+function twoToThree (slide, delay = 0) {
+
+    gsap.to(slide, {
+        x:percentWidthToPixel(slide.parentNode, 50) - percentWidthToPixel(slide, 50),
+        width:percentWidthToPixel(slide.parentNode, 20),
+        duration: 1,
+        delay: delay,
+        ease: "power1"
+    })
+
+}
+function threeToZero (slide, delay = 0) {
+    gsap.to(slide, {
+        x:0,
+        duration: 1,
+        delay: delay,
+        ease: "power1"
+    })
+}
+
+
 function percentWidthToPixel(_elem, _perc){
     return (_elem.offsetWidth/100)* parseFloat(_perc);
 }
-
-slideRightButton.addEventListener('click', turnSlideRight)
 
 function turnSlideRight () {
     let slideshowImageOne = document.querySelector(".slideshow-image-1");
@@ -63,3 +125,32 @@ function turnSlideRight () {
     }, 4010);
 }
 
+function slideshowRight () {
+    let slideshowImageOne = document.querySelector(".slideshow-image-1");
+    let slideshowImageTwo = document.querySelector(".slideshow-image-2");
+    let slideshowImageThree = document.querySelector(".slideshow-image-3");
+
+    slideshowImageOne.classList.remove("slideshow-image-1");
+    slideshowImageOne.classList.add("slideshow-image-2");
+    slideshowImageOne.style.transform = null;
+    slideshowImageOne.style.width = null;
+    slideshowImageTwo.classList.remove("slideshow-image-2");
+    slideshowImageTwo.classList.add("slideshow-image-3");
+    slideshowImageTwo.style.transform = null;
+    slideshowImageTwo.style.width = null;
+    slideshowImageThree.classList.remove("slideshow-image-3");
+    slideshowImageThree.classList.add("slideshow-image-1");
+    slideshowImageThree.style.transform = null;
+    slideshowImageThree.style.width = null;
+
+    gsap.from(".slideshow-image-2", {
+        duration:4,
+        x:-1 * (percentWidthToPixel(slideshowImageOne.parentNode, 100) - percentWidthToPixel(slideshowImageOne, 100)),
+        width:percentWidthToPixel(slideshowImageOne.parentNode, 50),
+        ease:"power1"
+    })
+    
+}
+
+
+slideRightButton.addEventListener('click', startSlideShow)
