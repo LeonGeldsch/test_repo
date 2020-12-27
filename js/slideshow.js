9d45cf3b4428fee17c8e05c2fd923c46a98dc3d4
LeonGeldsch <leon.geldschlaeger@gmail.com>
2020-12-27 21:38:26 +0100

    updated slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,15 +1,15 @@
-var slideRightButton = document.querySelector(".btn-right");
-var slideLeftButton = document.querySelector(".btn-left");
+var slideRightButton = document.querySelector(".btn-right")
+var slideLeftButton = document.querySelector(".btn-left")
 
 var allSlides = document.querySelectorAll(".slideshow-image")
 
-var tl = gsap.timeline();
+var tl = gsap.timeline()
 
-var firstSlideIndex = 0;
+var firstSlideIndex = 0
 
 
 function turnSlideRight () {
-    console.log(firstSlideIndex);
+    console.log(firstSlideIndex)
         zeroToOne(allSlides[firstSlideIndex % allSlides.length])
         oneToTwo(allSlides[(firstSlideIndex+3) % allSlides.length])
         twoToThree(allSlides[(firstSlideIndex+2) % allSlides.length])
@@ -19,11 +19,14 @@ function turnSlideRight () {
 
 function turnSlideLeft () {
     console.log(firstSlideIndex);
-        zeroToOne(allSlides[firstSlideIndex % allSlides.length])
-        oneToTwo(allSlides[(firstSlideIndex+3) % allSlides.length])
-        twoToThree(allSlides[(firstSlideIndex+2) % allSlides.length])
-        threeToZero(allSlides[(firstSlideIndex+1) % allSlides.length])
-        firstSlideIndex--
+    twoToOne(allSlides[(firstSlideIndex+2) % allSlides.length]) // 1
+    oneToTwo(allSlides[(firstSlideIndex+1) % allSlides.length]) // 0
+    zeroToThree(allSlides[(firstSlideIndex) % allSlides.length]) // 3
+    threeToZero(allSlides[(firstSlideIndex+3) % allSlides.length]) // 2
+    firstSlideIndex--
+    if (firstSlideIndex === 0) {
+        firstSlideIndex = allSlides.length * 10
+    }
 }
 
 
@@ -45,16 +48,19 @@ function startSlideShow () {
 
 
 function percentWidthToPixel(_elem, _perc){
-    return (_elem.offsetWidth/100)* parseFloat(_perc);
+    return (_elem.offsetWidth/100)* parseFloat(_perc)
 }
 
 function zeroToOne (slide, delay = 0) {
     gsap.to(slide, {
         x:-percentWidthToPixel(slide.parentNode, 50) + percentWidthToPixel(slide, 50),
+        y:percentWidthToPixel(slide.parentNode, 2),
+        width:percentWidthToPixel(slide.parentNode, 20),
         duration: 1,
         delay: delay,
         ease: "power1"
     })
+    console.log(-percentWidthToPixel(slide.parentNode, 2))
 }
 function oneToTwo (slide, delay = 0) {
     gsap.set(slide, {
@@ -63,29 +69,35 @@ function oneToTwo (slide, delay = 0) {
     })
     gsap.to(slide, {
         x:0,
+        y:percentWidthToPixel(slide.parentNode, 3),
         width:percentWidthToPixel(slide.parentNode, 50),
         duration: 1,
         delay: delay,
         ease: "power1"
     })
+    console.log(-percentWidthToPixel(slide.parentNode, 2))
 }
 function twoToThree (slide, delay = 0) {
     gsap.to(slide, {
         x:percentWidthToPixel(slide.parentNode, 50) - percentWidthToPixel(slide, 50) / 2.5,
+        y:percentWidthToPixel(slide.parentNode, 2),
         width:percentWidthToPixel(slide.parentNode, 20),
         duration: 1,
         delay: delay,
         ease: "power1"
     })
+    console.log(-percentWidthToPixel(slide.parentNode, 2))
 }
 function twoToThreeStart (slide, delay = 0) {
     gsap.to(slide, {
         x:percentWidthToPixel(slide.parentNode, 50) - percentWidthToPixel(slide, 50),
+        y:percentWidthToPixel(slide.parentNode, 2),
         width:percentWidthToPixel(slide.parentNode, 20),
         duration: 1,
         delay: delay,
         ease: "power1"
     })
+    console.log(-percentWidthToPixel(slide.parentNode, 2))
 }
 function threeToZero (slide, delay = 0) {
     gsap.set(slide, {
@@ -94,11 +106,36 @@ function threeToZero (slide, delay = 0) {
     })
     gsap.to(slide, {
         x:0,
+        y:0,
+        duration: 1,
+        delay: delay,
+        ease: "power1"
+    })
+    console.log(-percentWidthToPixel(slide.parentNode, 2))
+}
+
+
+function twoToOne(slide, delay = 0) {
+    gsap.to(slide, {
+        x:-percentWidthToPixel(slide.parentNode, 50) + percentWidthToPixel(slide, 50) / 2.5,
+        y:percentWidthToPixel(slide.parentNode, 2),
+        width:percentWidthToPixel(slide.parentNode, 20),
         duration: 1,
         delay: delay,
         ease: "power1"
     })
 }
+function zeroToThree(slide, delay = 0) {
+    gsap.to(slide, {
+        x:percentWidthToPixel(slide.parentNode, 50) - percentWidthToPixel(slide, 50),
+        y:percentWidthToPixel(slide.parentNode, 2),
+        width:percentWidthToPixel(slide.parentNode, 20),
+        duration: 1,
+        delay: delay,
+        ease: "power1"
+    })
+}
+
 
 
 
