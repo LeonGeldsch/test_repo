47aa15d930fb49576030b7b995f8fdd131d0cc5f
LeonGeldsch <leon.geldschlaeger@gmail.com>
2021-01-26 18:27:10 +0100

    updated footer animation

--- js/scrolltrigger.js
+++ js/scrolltrigger.js
@@ -42,44 +42,12 @@ gsap.from("#section3 .text-wrapper", {
         }
 })
 
-/*
-// fly in 1
-gsap.from(".fly-in-1", {
-    duration:1, y:"200px", opacity:1, ease:"bounce.in",
-        scrollTrigger: {
-            trigger:"#footer-scrolltrigger"
-        }
-})
-gsap.from(".fly-in-1", {
-    duration:1, x:"200px", opacity:0, ease:"bounce.out",
-        scrollTrigger: {
-            trigger:"#footer-scrolltrigger"
-        }
-})
-
-// fly-in-2
-gsap.from(".fly-in-2", {
-    duration:1, y:"-300px", opacity:1, ease:"bounce.out",
-        scrollTrigger: {
-            trigger:"#footer-scrolltrigger"
-        }
-})
-gsap.from(".fly-in-2", {
-    duration:1, x:"-100px", opacity:0, ease:"power2",
-        scrollTrigger: {
-            trigger:"#footer-scrolltrigger"
-        }
-})
-*/
-
-
 
 
 var allFadeInSideElements = gsap.utils.toArray('footer p');
 
 
 for (i = 0; i < allFadeInSideElements.length; i++) {
-    console.log(i % 2);
     if (i % 2 == 0) {
         gsap.from(allFadeInSideElements[i], {
             duration:1.5,
@@ -102,16 +70,3 @@ for (i = 0; i < allFadeInSideElements.length; i++) {
         })
     }
 }
-
-/*
-allFadeInLeft.forEach((element) => {
-    gsap.from(element, {
-        duration:2,
-        x:"-300px",
-        opacity:0,
-        scrollTrigger: {
-            trigger: link
-        }
-    })
-})
-*/
